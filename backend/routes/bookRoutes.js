233022bdcafb9af4d21852a661aad3bf3e263796
Sankalpa <joshisankalpa2@gmail.com>
2024-09-01 21:33:37 +0545

    book add and view api complted

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,14 +1,16 @@
-// routes/book.js
 module.exports = function(upload) {
   const express = require('express');
   const router = express.Router();
   const Book = require('../models/Book');
 
   // Add a new book
-  router.post('/add', async (req, res) => {
-    const { title, author, isbn, quantity, status } = req.body;
+  router.post('/add', upload.single('image'), async (req, res) => {
+    const { title, author, isbn, quantity, status, publishedDate } = req.body; // Add publishedDate here
+    
+    // Handle uploaded file
+    const imageUrl = req.file ? `/uploads/${req.file.filename}` : null;
   
-    if (!title || !author || !isbn || !quantity || !status) {
+    if (!title || !author || !isbn || !quantity || !status || !publishedDate) {
       return res.status(400).json({ message: 'Please fill in all required fields.' });
     }
   
@@ -19,7 +21,8 @@ module.exports = function(upload) {
         isbn,
         quantity,
         status,
-        image: null // Skip image for now
+        publishedDate, // Include publishedDate in the creation
+        images: imageUrl
       });
       res.status(201).json(newBook);
     } catch (error) {
@@ -28,7 +31,16 @@ module.exports = function(upload) {
     }
   });
   
-  
+
+  router.get('/view', async (req, res) => {
+    try {
+      const books = await Book.findAll();
+      res.status(200).json(books);
+    } catch (error) {
+      console.error('Error while fetching books:', error.message);
+    res.status(500).json({ message: 'Error fetching books', error: error.message });
+    }
+  });
 
   return router;
 };
--- frontend/src/pages/admin/ViewBooks.jsx
+++ frontend/src/pages/admin/ViewBooks.jsx
@@ -9,68 +9,49 @@ const ViewBooks = () => {
     title: '',
     author: '',
     isbn: '',
-    quantity: '',
-    status: 'Available',
-    image: null
+    publishedDate: '',
+    description: '',
+    quantity: 1,
+    status: 'available',
+    image: null,
   });
+
   const [error, setError] = useState('');
 
-  // Function to toggle the modal
-  const toggleModal = () => {
-    setShowModal(!showModal);
-  };
+  const toggleModal = () => setShowModal(!showModal);
 
-  // Handle form input changes
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
+    setFormData({ ...formData, [name]: value });
   };
 
-  // Handle file input change
   const handleFileChange = (e) => {
-    setFormData({
-      ...formData,
-      image: e.target.files[0]
-    });
+    setFormData({ ...formData, image: e.target.files[0] });
   };
 
-  // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    // Create form data to handle file upload
     const data = new FormData();
-    data.append('title', formData.title);
-    data.append('author', formData.author);
-    data.append('isbn', formData.isbn);
-    data.append('quantity', formData.quantity);
-    data.append('status', formData.status);
-    if (formData.image) {
-      data.append('image', formData.image);
-    }
-    for (let [key, value] of data.entries()) {
-      console.log(`${key}: ${value}`);
-    }
-  
+    Object.keys(formData).forEach(key => {
+      if (key !== 'image') data.append(key, formData[key]);
+    });
+    if (formData.image) data.append('image', formData.image);
+
     try {
       const response = await axios.post('http://localhost:5000/api/books/add', data, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
+        headers: { 'Content-Type': 'multipart/form-data' }
       });
       console.log('Book added:', response.data);
-      toggleModal(); // Close the modal on success
-      // Reset the form
+      toggleModal();
       setFormData({
         title: '',
         author: '',
         isbn: '',
-        quantity: '',
-        status: 'Available',
-        image: null
+        publishedDate: '',
+        description: '',
+        quantity: 1,
+        status: 'available',
+        image: null,
       });
     } catch (error) {
       console.error('Error adding book:', error);
@@ -80,39 +61,38 @@ const ViewBooks = () => {
 
   return (
     <div className="bg-white p-6 rounded shadow-md">
-      <div className='flex justify-between items-center'>
-        <h2 className="text-2xl font-bold mb-4">List of Books</h2>
-        <button 
-          onClick={toggleModal} 
-          className='bg-blue-700 text-white p-2 gap-x-2 rounded-md flex items-center text-center'
-        >
-          <IoMdAddCircle />
-          Add Books
+      <div className='flex justify-between items-center '>
+        <h2 className="text-2xl font-bold">List of Books</h2>
+        <button onClick={toggleModal} className='bg-blue-700 text-white p-2 rounded-md flex items-center'>
+          <IoMdAddCircle className="mr-2" /> Add Book
         </button>
       </div>
 
-      <table className="min-w-full bg-white border-collapse mt-2">
+      <table className="min-w-full bg-white border-collapse mt-6">
         <thead>
           <tr className="bg-orange-100">
-            <th className="py-2 px-4 border border-gray-300 text-center text-gray-600">ID</th>
-            <th className="py-2 px-4 border border-gray-300 text-center text-gray-600">Image</th>
-            <th className="py-2 px-4 border border-gray-300 text-center text-gray-600">Title</th>
-            <th className="py-2 px-4 border border-gray-300 text-center text-gray-600">Author</th>
-            <th className="py-2 px-4 border border-gray-300 text-center text-gray-600">ISBN</th>
-            <th className="py-2 px-4 border border-gray-300 text-center text-gray-600">Quantity</th>
-            <th className="py-2 px-4 border border-gray-300 text-center text-gray-600">Status</th>
-            <th className="py-2 px-4 border border-gray-300 text-center text-gray-600">Action</th>
+            <th className="py-2 px-4 border border-gray-300 text-center">ID</th>
+            <th className="py-2 px-4 border border-gray-300 text-center">Image</th>
+            <th className="py-2 px-4 border border-gray-300 text-center">Title</th>
+            <th className="py-2 px-4 border border-gray-300 text-center">Author</th>
+            <th className="py-2 px-4 border border-gray-300 text-center">ISBN</th>
+            <th className="py-2 px-4 border border-gray-300 text-center">Published Date</th>
+            <th className="py-2 px-4 border border-gray-300 text-center">Quantity</th>
+            <th className="py-2 px-4 border border-gray-300 text-center">Status</th>
+            <th className="py-2 px-4 border border-gray-300 text-center">Action</th>
           </tr>
         </thead>
         <tbody>
+          {/* Example row, replace with dynamic data */}
           <tr>
             <td className="py-2 px-4 border border-gray-300 text-center">1</td>
             <td className="py-2 px-4 border border-gray-300 text-center">
-              <img src="path/to/book-image.jpg" alt="Book" className="h-16 w-12 object-cover mx-auto"/>
+              <img src="path/to/book-image.jpg" alt="Book" className="h-16 w-12 object-cover mx-auto" />
             </td>
             <td className="py-2 px-4 border border-gray-300 text-center">The Great Gatsby</td>
             <td className="py-2 px-4 border border-gray-300 text-center">F. Scott Fitzgerald</td>
             <td className="py-2 px-4 border border-gray-300 text-center">9780743273565</td>
+            <td className="py-2 px-4 border border-gray-300 text-center">1925-04-10</td>
             <td className="py-2 px-4 border border-gray-300 text-center">10</td>
             <td className="py-2 px-4 border border-gray-300 text-center">Available</td>
             <td className="py-2 px-4 border border-gray-300 text-center">
@@ -125,91 +105,111 @@ const ViewBooks = () => {
         </tbody>
       </table>
 
-      {/* Modal for Adding Books */}
       {showModal && (
         <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-            <h3 className="text-xl font-bold mb-4">Add New Book</h3>
-            {error && <p className="text-red-500 mb-4">{error}</p>}
+          <div className="bg-white p-6 rounded-lg shadow-lg max-w-lg w-full">
+            <h3 className="text-xl font-bold ">Add New Book</h3>
+            {error && <p className="text-red-500 ">{error}</p>}
             <form onSubmit={handleSubmit}>
-              <div className="mb-4">
-                <label className="block text-gray-700">Title</label>
-                <input 
-                  type="text" 
-                  name="title"
-                  value={formData.title}
-                  onChange={handleInputChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1" 
-                  placeholder="Book Title"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700">Author</label>
-                <input 
-                  type="text" 
-                  name="author"
-                  value={formData.author}
-                  onChange={handleInputChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1" 
-                  placeholder="Author Name"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700">ISBN</label>
-                <input 
-                  type="text" 
-                  name="isbn"
-                  value={formData.isbn}
-                  onChange={handleInputChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1" 
-                  placeholder="ISBN Number"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700">Quantity</label>
-                <input 
-                  type="number" 
-                  name="quantity"
-                  value={formData.quantity}
-                  onChange={handleInputChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1" 
-                  placeholder="Quantity"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700">Status</label>
-                <select 
-                  name="status"
-                  value={formData.status}
-                  onChange={handleInputChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1"
-                >
-                  <option>Available</option>
-                  <option>Not Available</option>
-                </select>
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700">File or Media</label>
-                <input 
-                  type="file" 
-                  onChange={handleFileChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1"
-                />
-              </div>
-              <div className="flex justify-end space-x-2">
-                <button 
-                  type="button" 
-                  onClick={toggleModal} 
-                  className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
-                >
-                  Cancel
-                </button>
-                <button 
-                  type="submit" 
-                  className="bg-blue-700 text-white px-4 py-2 rounded hover:bg-blue-600"
-                >
-                  Add Book
-                </button>
+              <div className="grid grid-cols-1 gap-4">
+                <div className="">
+                  <input
+                    type="text"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleInputChange}
+                    className="w-full p-2 border border-gray-300 rounded"
+                    placeholder="Book Title"
+                    required
+                  />
+                </div>
+                <div className="">
+                  <input
+                    type="text"
+                    name="author"
+                    value={formData.author}
+                    onChange={handleInputChange}
+                    className="w-full p-2 border border-gray-300 rounded"
+                    placeholder="Author Name"
+                    required
+                  />
+                </div>
+                <div className="">
+                  <input
+                    type="text"
+                    name="isbn"
+                    value={formData.isbn}
+                    onChange={handleInputChange}
+                    className="w-full p-2 border border-gray-300 rounded"
+                    placeholder="ISBN Number"
+                    required
+                  />
+                </div>
+                <div className="">
+                  <input
+                    type="date"
+                    name="publishedDate"
+                    value={formData.publishedDate}
+                    onChange={handleInputChange}
+                    className="w-full p-2 border border-gray-300 rounded"
+                    required
+                  />
+                </div>
+                <div className="">
+                  <textarea
+                    name="description"
+                    value={formData.description}
+                    onChange={handleInputChange}
+                    className="w-full p-2 border border-gray-300 rounded"
+                    placeholder="Book Description"
+                  />
+                </div>
+                <div className="">
+                  <input
+                    type="number"
+                    name="quantity"
+                    value={formData.quantity}
+                    onChange={handleInputChange}
+                    className="w-full p-2 border border-gray-300 rounded"
+                    placeholder="Quantity"
+                    min="1"
+                    required
+                  />
+                </div>
+                <div className="">
+                  <select
+                    name="status"
+                    value={formData.status}
+                    onChange={handleInputChange}
+                    className="w-full p-2 border border-gray-300 rounded"
+                  >
+                    <option value="available">Available</option>
+                    <option value="not available">Not Available</option>
+                  </select>
+                </div>
+                <div className="">
+                  <label className="block text-gray-700 font-medium mb-1">Book Image</label>
+                  <input
+                    type="file"
+                    onChange={handleFileChange}
+                    className="w-full p-2 border border-gray-300 rounded"
+                  />
+                </div>
+                <div className="flex justify-end space-x-2">
+                  <button
+                    type="button"
+                    onClick={toggleModal}
+                    className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    className="bg-blue-700 text-white px-4 py-2 rounded hover:bg-blue-600"
+                  >
+                    Add Book
+                  </button>
+                </div>
               </div>
             </form>
           </div>
